8ecef2976427b88c82df02c8d06a4629f131d353
dewa99 <dodeardi@gmail.com>
2020-02-25 19:13:06 +0700

    first

---
+++ App.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import {StyleSheet, Text, View,Button} from 'react-native';
+import { createBottomTabNavigator, createAppContainer} from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+class HomeScreen extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text>Home Screen</Text>
+        </View>
+    );
+  }
+}
+class ProfileScreen extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text>Profile Screen</Text>
+        </View>
+    );
+  }
+}
+class ImageScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Image Screen</Text>
+            </View>
+        );
+    }
+}
+class CartScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Cart Screen</Text>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+});
+const TabNavigator = createMaterialBottomTabNavigator(
+    {
+        Home: { screen: HomeScreen,
+            navigationOptions:{
+                tabBarLabel:'Home',
+                tabBarIcon: ({ tintColor }) => (
+                    <View>
+                        <Icon style={[{color: tintColor}]} size={25} name={'home'}/>
+                    </View>),
+            }
+        },
+        Profile: { screen: ProfileScreen,
+            navigationOptions:{
+                tabBarLabel:'Browse',
+                tabBarIcon: ({ tintColor }) => (
+                    <View>
+                        <Icon style={[{color: tintColor}]} size={25} name={'search'}/>
+                    </View>),
+                activeColor: '#f60c0d',
+                inactiveColor: '#f65a22',
+                barStyle: { backgroundColor: '#f69b31' },
+            }
+        },
+        Image: { screen: ImageScreen,
+            navigationOptions:{
+                tabBarLabel:'Playlist',
+                tabBarIcon: ({ tintColor }) => (
+                    <View>
+                        <Icon style={[{color: tintColor}]} size={25} name={'list-ul'}/>
+                    </View>),
+                activeColor: '#615af6',
+                inactiveColor: '#46f6d7',
+                barStyle: { backgroundColor: '#67baf6' },
+            }
+        },
+        Cart: {
+            screen: CartScreen,
+            navigationOptions:{
+                tabBarLabel:'Settings',
+                tabBarIcon: ({ tintColor }) => (
+                    <View>
+                        <Icon style={[{color: tintColor}]} size={25} name={'gear'}/>
+                    </View>),
+            }
+        },
+    },
+    {
+      initialRouteName: "Home",
+      activeColor: '#f0edf6',
+      inactiveColor: '#226557',
+      barStyle: { backgroundColor: '#3BAD87' },
+    },
+);
+
+export default createAppContainer(TabNavigator);
